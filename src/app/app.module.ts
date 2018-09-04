7a1124f043b6f31c478a3f5535648e30d4ad088a
Choudhary <juhi.choudhary@capgemini.com>
2018-09-05 01:18:21 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { AppErrorHandler } from './common/app-error-handler';
+import { PostService } from './services/post.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, ErrorHandler } from '@angular/core';
+import { HttpModule } from '@angular/http';
+
+
+import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PostsComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule    
+  ],
+  providers: [
+    PostService,
+    { provide: ErrorHandler, useClass: AppErrorHandler} 
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
